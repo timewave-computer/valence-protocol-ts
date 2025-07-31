6e02a8ee4be43bcb71ceda0fdad43d7644f30a85
Lena <elena.mikhaylova0@gmail.com>
2025-07-31 15:14:09 -0400

    rework evm client

--- packages/domain-clients-react/src/evm/hooks/useEvmClient.tsx
+++ packages/domain-clients-react/src/evm/hooks/useEvmClient.tsx
@@ -1,27 +1,25 @@
 import { useEffect, useMemo } from 'react';
 import { useEvmClientStore, useEvmConfig } from '@/evm';
 import { EvmClient } from '@valence-protocol/domain-clients-core/evm';
-import { useAccount, useDisconnect, useWalletClient } from 'wagmi';
+import { useAccount, usePublicClient } from 'wagmi';
 
 export function useEvmClient() {
   const config = useEvmConfig();
   const account = useAccount();
-  const {disconnect} = useDisconnect();
-  const { data: walletClient } = useWalletClient();
+  const publicClient  = usePublicClient();
 
   const {client, setClient} = useEvmClientStore();
 
   useEffect(() => {
-    if (!account || !walletClient) return;
+    if (!account || !publicClient) return;
     const client = new EvmClient(config);
     setClient(client);
 
-  }, [account, walletClient, config]);
+  }, [account, publicClient, config, setClient]);
 
   return useMemo(() => ({
     client,
     account,
-    disconnect,
-  }), [client, account, disconnect]);
+  }), [client, account]);
 
 } 
