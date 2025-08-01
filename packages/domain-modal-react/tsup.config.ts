df0e61d293b10f2b5f3839680052a5c7824810de
Lena <elena.mikhaylova0@gmail.com>
2025-08-01 14:37:02 -0400

    fix tsup filename for modal

--- packages/domain-modal-react/tsup.config.ts
+++ packages/domain-modal-react/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: {
-    'index': 'src/index.tsx',
+    'index': 'src/index.ts',
   },
   outDir: 'dist', // output directory
   format: ['esm', 'cjs'],
