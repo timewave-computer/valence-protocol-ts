76db84459649fdb5bb927b0b548a3c157e4c7c0d
Lena <elena.mikhaylova0@gmail.com>
2025-08-07 15:19:30 -0400

    fix build error

--- apps/domain-clients-example/src/config/domainClientsConfig/evm.config.ts
+++ apps/domain-clients-example/src/config/domainClientsConfig/evm.config.ts
@@ -1,13 +1,13 @@
 import {  mainnet } from "wagmi/chains";
-import {  http } from "wagmi";
+import {  http, } from "wagmi";
 import { Chain, createClient } from "viem";
 
-import {  createEvmConfig } from "@valence-protocol/domain-clients-core";
+import { createEvmConfig } from "@valence-protocol/domain-clients-core";
 
 export const evmConfig =  createEvmConfig({
   chains: [mainnet],
   ssr: true, // required for Next.js, prevents hydration errors
-  client({ chain }: { chain: Chain }) {
+  client: ({ chain }) => {
     return createClient({
       chain,
       transport: http(chain.rpcUrls.default.http[0]),
