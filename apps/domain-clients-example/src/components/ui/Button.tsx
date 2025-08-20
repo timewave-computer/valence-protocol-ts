5c9396a685500fc52d2f0c690226c671894b10b7
Lena <elena.mikhaylova0@gmail.com>
2025-08-20 13:06:34 -0400

    button disabled style

--- apps/domain-clients-example/src/components/ui/Button.tsx
+++ apps/domain-clients-example/src/components/ui/Button.tsx
@@ -1,13 +1,14 @@
 import { cva, type VariantProps } from 'class-variance-authority';
 
 const buttonVariants = cva(
-  'px-2 py-1 rounded-sm font-semibold transition-colors duration-200 border outline-none rounded-xs border cursor-pointer text-sm ',
+  'px-2 py-1 rounded-sm font-semibold transition-colors duration-200 border outline-none rounded-xs border text-sm ',
   {
     variants: {
       variant: {
-        default: 'bg-gray-900 text-white hover:bg-gray-700',
-        secondary: 'border-gray-700 hover:bg-gray-100',
-        disabled: 'border-gray-500 cursor-not-allowed text-white bg-gray-500',
+        default: 'bg-gray-900 text-white hover:bg-gray-600 cursor-pointer',
+        secondary: 'border-gray-700 hover:bg-gray-100 cursor-pointer',
+        disabled:
+          'border-gray-300 cursor-not-allowed text-gray-900 bg-gray-300',
       },
     },
   }
