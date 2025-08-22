7cc161e01ffa52a44fcaad91e07a97f3c4688090
Lena <elena.mikhaylova0@gmail.com>
2025-08-22 11:07:03 -0400

    remove use of chain client, base client

--- packages/domain-clients-core/src/solana/BaseClient.ts
+++ packages/domain-clients-core/src/solana/BaseClient.ts
@@ -1,5 +1,25 @@
-import { ChainClient } from '@/common';
+import { Blockhash, createSolanaClient, SolanaClusterMoniker } from 'gill';
 
-export abstract class SolanaBaseClient extends ChainClient {
-  public abstract queryLatestBlockHash(): Promise<string>;
+export interface SolanaBaseClientArgs {
+  rpcUrlOrMoniker: SolanaClusterMoniker | string;
+}
+
+export class SolanaBaseClient {
+  public readonly rpcUrlOrMoniker: SolanaClusterMoniker | string;
+
+  constructor(args: SolanaBaseClientArgs) {
+    this.rpcUrlOrMoniker = args.rpcUrlOrMoniker;
+  }
+
+  public getClient() {
+    return createSolanaClient({
+      urlOrMoniker: this.rpcUrlOrMoniker,
+    });
+  }
+
+  public async queryLatestBlockHash() {
+    const client = this.getClient();
+    const response = await client.rpc.getLatestBlockhash().send();
+    return response.value;
+  }
 }
